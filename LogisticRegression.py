c8e6f7cc7fbafc3c09851341261bead1d43a3c2f
Gulce62 <98024902+Gulce62@users.noreply.github.com>
2022-12-05 02:02:18 +0300

    Added final arrangements

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,18 +1,7 @@
 import numpy as np
-import copy
-from sklearn.metrics import accuracy_score
-
-
-def gaussianInitialization(dimension):
-    np.random.seed(9)
-    w = np.random.normal(loc=0, scale=1, size=(dimension, 1))
-    return w
-
-
-def zeroInitialization(dimension):
-    np.random.seed(9)
-    w = np.zeros((dimension, 1))
-    return w
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
 
 
 def sigmoid(z):
@@ -20,32 +9,66 @@ def sigmoid(z):
     return s
 
 
+def gaussianInitialization(dimension):
+    np.random.seed(50)
+    p = np.random.normal(loc=0, scale=1, size=(dimension, 1))
+    return p
+
+
+def uniformInitialization(dimension):
+    np.random.seed(50)
+    p = np.random.uniform(size=(dimension, 1))
+    return p
+
+
+def zeroInitialization(dimension):
+    p = np.zeros((dimension, 1))
+    return p
+
+
+def getAccuracy(y_true, y_pred):
+    accuracyBool = (y_true.ravel() == y_pred.ravel())
+    accuracy = np.count_nonzero(accuracyBool) / accuracyBool.shape[0]
+    return accuracy
+
+
 class LogisticRegression:
-    def __init__(self, epochs=100, learningRate=0.001, batchSize=64):
+    def __init__(self, epochs=100, learningRate=0.001, batchSize=64, initialization='gaussian'):
         self.epochs = epochs
         self.learningRate = learningRate
         self.batchSize = batchSize
+        self.initialization = {'gaussian': lambda dimension: gaussianInitialization(dimension),
+                               'uniform': lambda dimension: uniformInitialization(dimension),
+                               'zero': lambda dimension: zeroInitialization(dimension)}[initialization]
 
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train, X_val, y_val):
         m = X_train.shape[0]
-        w = zeroInitialization(X_train.shape[1])
-        b = zeroInitialization(1)
+        w = self.initialization(X_train.shape[1])
+        b = self.initialization(1)
+        if m % self.batchSize == 0:
+            iterationNo = m // self.batchSize
+        else:
+            iterationNo = m // self.batchSize + 1
+        val_acc = []
         for epoch in range(self.epochs):
-            for batch in range(m // self.batchSize + 1):
+            for batch in range(iterationNo):
                 startIdx = batch * self.batchSize
-                endIdx = 2 * batch * self.batchSize
-                if batch == m // self.batchSize:
+                endIdx = startIdx + self.batchSize
+                if batch == iterationNo - 1:
+                    prob = sigmoid(np.dot(X_train[startIdx:], w) + b)
+                    dw = (1 / self.batchSize) * np.dot(X_train[startIdx:].T, (prob - y_train[startIdx:]))
+                    db = (1 / self.batchSize) * np.sum(prob - y_train[startIdx:])
+                else:
                     prob = sigmoid(np.dot(X_train[startIdx:endIdx], w) + b)
                     dw = (1 / self.batchSize) * np.dot(X_train[startIdx:endIdx].T, (prob - y_train[startIdx:endIdx]))
                     db = (1 / self.batchSize) * np.sum(prob - y_train[startIdx:endIdx])
-                else:
-                    prob = sigmoid(np.dot(X_train[startIdx:], w) + b)
-                    dw = (1 / self.batchSize) * np.dot(X_train[startIdx:].T, (prob - y_train[startIdx:]))
-                    db = (1 / self.batchSize) * np.sum(prob - y_train[batch])
                 w -= self.learningRate * dw
                 b -= self.learningRate * db
-            print('-------------- Epoch', epoch, 'finished --------------')
-        return w, b
+            y_pred = self.predict(w, b, X_val, 0.5)
+            val_accuracy = getAccuracy(y_pred, y_val)
+            val_acc.append(val_accuracy)
+            print('Epoch', epoch, 'finished. Accuracy is', val_accuracy)
+        return w, b, val_acc
 
     def predict(self, w, b, X, threshold):
         y_pred = np.zeros(X.shape[0])
--- main.py
+++ main.py
@@ -24,9 +24,11 @@ def trainValTestSplit(data):
     test = shuffled[int(dataSize * 0.8):]
     return train, val, test
 
+
 def normalize(X, min, max):
     X = (X - min) / (max - min)
     return X
+
 
 def get_data(file_location):
     data = read_data(file_location)
@@ -46,6 +48,22 @@ def get_data(file_location):
     return X_train, X_val, X_test, y_train, y_val, y_test
 
 
+def getConfusionMatrix(y_true, y_pred, axs):
+    y_true = pd.Categorical(y_true.ravel())
+    y_pred = pd.Categorical(y_pred.ravel())
+    confusion_matrix = pd.crosstab(y_true, y_pred, rownames=['Actual'], colnames=['Predicted'], dropna=False)
+    print(confusion_matrix)
+    sn.heatmap(confusion_matrix, cmap="Blues", annot=True, ax=axs)
+
+
+def getValAccPlot(val_acc, paramaterName):
+    title = 'Validation Accuracy For Every Epoch With Different ' + paramaterName
+    plt.title(title)
+    plt.xlabel('Epoch')
+    plt.ylabel('Validation Accuracy')
+    plt.plot(val_acc)
+
+
 absolutePath = r'C:\Users\gulce\Desktop\EEE 8TH SEMESTER\CS 464\Homeworks\HW2\dataset.csv'
 # absolutePath = input('Enter the file location of the dataset: ')
 X_train, X_val, X_test, y_train, y_val, y_test = get_data(absolutePath)
@@ -58,10 +76,86 @@ print("y_val shape: {}".format(y_val.shape))
 print("y_test shape: {}".format(y_test.shape))
 
 batchSize = [42000, 64, 1]
+batchSizeAcc = []
+y_predList = []
+plt.figure()
 for size in batchSize:
-    print('\nStart training for batchsize =', size, '\n')
-    model = LR.LogisticRegression(epochs=100, learningRate=0.001, batchSize=size)
-    w, b = model.fit(X_train, y_train)
+    print('\nStart training for batch size =', size, '\n')
+    model = LR.LogisticRegression(epochs=100, learningRate=0.001, batchSize=size, initialization='gaussian')
+    w, b, val_acc = model.fit(X_train, y_train, X_val, y_val)
+    getValAccPlot(val_acc, 'Batch Size')
     y_pred = model.predict(w, b, X_val, 0.5)
-    print('\nAcc for batchsize =', size, 'is:', accuracy_score(y_pred, y_val))
+    y_predList.append(y_pred)
+    accuracy = LR.getAccuracy(y_pred, y_val)
+    batchSizeAcc.append(accuracy)
+    print('\nFinal accuracy for batch size =', size, 'is:', accuracy)
+plt.legend(str(batchSize))
+plt.show()
 
+fig, axs = plt.subplots(1, 3, figsize=(16, 16))
+for i in range(0, 3):
+    title = 'Confusion Matrix for batch size =' + str(batchSize[i])
+    plt.title(title)
+    getConfusionMatrix(y_predList[i], y_val, axs[i])
+plt.show()
+
+initializationType = ['gaussian', 'uniform', 'zero']
+initializationAcc = []
+y_predList = []
+plt.figure()
+for inType in initializationType:
+    print('\nStart training for', inType, 'initialization\n')
+    model = LR.LogisticRegression(epochs=100, learningRate=0.001, batchSize=64, initialization=inType)
+    w, b, val_acc = model.fit(X_train, y_train, X_val, y_val)
+    getValAccPlot(val_acc, 'Initialization Type')
+    y_pred = model.predict(w, b, X_val, 0.5)
+    y_predList.append(y_pred)
+    accuracy = LR.getAccuracy(y_pred, y_val)
+    initializationAcc.append(accuracy)
+    print('\nFinal accuracy for for', inType, 'initialization is:', accuracy)
+plt.legend(str(initializationType))
+plt.show()
+
+fig, axs = plt.subplots(1, 3, figsize=(32, 8))
+for i in range(0, 3):
+    title = 'Confusion Matrix for' + str(initializationType[i]) + 'initialization'
+    plt.title(title)
+    getConfusionMatrix(y_predList[i], y_val, axs[i])
+plt.show()
+
+learningRate = [0.001, 0.0001, 0.00001]
+learningRateAcc = []
+y_predList = []
+plt.figure()
+for rate in learningRate:
+    print('\nStart training for learning rate =', rate, '\n')
+    model = LR.LogisticRegression(epochs=100, learningRate=rate, batchSize=64, initialization='gaussian')
+    w, b, val_acc = model.fit(X_train, y_train, X_val, y_val)
+    getValAccPlot(val_acc, 'Learning Rate')
+    y_pred = model.predict(w, b, X_val, 0.5)
+    y_predList.append(y_pred)
+    accuracy = LR.getAccuracy(y_pred, y_val)
+    learningRateAcc.append(accuracy)
+    print('\nFinal accuracy for learning rate =', rate, 'is:', accuracy)
+plt.legend(str(learningRate))
+plt.show()
+
+fig, axs = plt.subplots(1, 3, figsize=(32, 8))
+for i in range(0, 3):
+    title = 'Confusion Matrix for learning rate =' + str(learningRate[i])
+    plt.title(title)
+    getConfusionMatrix(y_predList[i], y_val, axs[i])
+plt.show()
+
+print('\nStart final training with best parameters\n')
+maxBatchSize = batchSize[np.argmax(batchSizeAcc)]
+maxInitialization = initializationType[np.argmax(initializationAcc)]
+maxLearningRate = learningRate[np.argmax(learningRateAcc)]
+model = LR.LogisticRegression(epochs=100, learningRate=maxLearningRate, batchSize=maxBatchSize,
+                              initialization=maxInitialization)
+w, b, val_acc = model.fit(X_train, y_train, X_val, y_val)
+y_pred = model.predict(w, b, X_test, 0.5)
+accuracy = LR.getAccuracy(y_pred, y_test)
+print('\nAccuracy for test set is:', accuracy)
+fig, axs = plt.subplots(1, 1, figsize=(32, 8))
+getConfusionMatrix(y_pred, y_test, axs)
