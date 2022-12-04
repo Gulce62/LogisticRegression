6b10a509f36ca78b8cc9122d27d4af0dfcd548c7
Gulce62 <98024902+Gulce62@users.noreply.github.com>
2022-12-04 17:34:45 +0300

    Added different batch sizes

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,40 +1,58 @@
 import numpy as np
 import copy
+from sklearn.metrics import accuracy_score
 
-def gaussianInitialization():
-    pass
+
+def gaussianInitialization(dimension):
+    np.random.seed(9)
+    w = np.random.normal(loc=0, scale=1, size=(dimension, 1))
+    return w
+
 
 def zeroInitialization(dimension):
+    np.random.seed(9)
     w = np.zeros((dimension, 1))
     return w
+
 
 def sigmoid(z):
     s = 1 / (1 + np.exp(-z))
     return s
 
+
 class LogisticRegression:
-    def __init__(self, epochs, learningRate):
+    def __init__(self, epochs=100, learningRate=0.001, batchSize=64):
         self.epochs = epochs
         self.learningRate = learningRate
+        self.batchSize = batchSize
 
     def fit(self, X_train, y_train):
-        w = zeroInitialization(X_train.shape[1])
         m = X_train.shape[0]
-        print(w.shape)
-        for e in range(self.epochs):
-            prob = 1-sigmoid(np.dot(X_train, w))
-            dw = (1 / m) * np.dot(X_train.T, (y_train-prob))
-            w += self.learningRate * dw
-        return w
+        w = zeroInitialization(X_train.shape[1])
+        b = zeroInitialization(1)
+        for epoch in range(self.epochs):
+            for batch in range(m // self.batchSize + 1):
+                startIdx = batch * self.batchSize
+                endIdx = 2 * batch * self.batchSize
+                if batch == m // self.batchSize:
+                    prob = sigmoid(np.dot(X_train[startIdx:endIdx], w) + b)
+                    dw = (1 / self.batchSize) * np.dot(X_train[startIdx:endIdx].T, (prob - y_train[startIdx:endIdx]))
+                    db = (1 / self.batchSize) * np.sum(prob - y_train[startIdx:endIdx])
+                else:
+                    prob = sigmoid(np.dot(X_train[startIdx:], w) + b)
+                    dw = (1 / self.batchSize) * np.dot(X_train[startIdx:].T, (prob - y_train[startIdx:]))
+                    db = (1 / self.batchSize) * np.sum(prob - y_train[batch])
+                w -= self.learningRate * dw
+                b -= self.learningRate * db
+            print('-------------- Epoch', epoch, 'finished --------------')
+        return w, b
 
-    def predict(self, w, X):
-        m = X.shape[0]
-        Y_prediction = np.zeros((1, m))
-        w = w.reshape(X.shape[1], 1)
-        A = sigmoid(np.dot(X, w))
-        for i in range(A.shape[1]):
-            if A[0, i] > 0.5:
-                Y_prediction[0, i] = 1
+    def predict(self, w, b, X, threshold):
+        y_pred = np.zeros(X.shape[0])
+        prob = sigmoid(np.dot(X, w) + b)
+        for i in range(prob.shape[0]):
+            if prob[i, 0] > threshold:
+                y_pred[i] = 1
             else:
-                Y_prediction[0, i] = 0
-        return Y_prediction
+                y_pred[i] = 0
+        return y_pred
--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sn
-import sklearn
-import sklearn.datasets
-import sklearn.linear_model as l
+from sklearn.metrics import accuracy_score
 import LogisticRegression as LR
 
 
@@ -19,7 +17,7 @@ def read_data(file_name):
 
 
 def trainValTestSplit(data):
-    shuffled = data.sample(frac=1, random_state=1)
+    shuffled = data.sample(frac=1, random_state=0)
     dataSize = len(shuffled)
     train = shuffled[:int(dataSize * 0.7)]
     val = shuffled[int(dataSize * 0.7):int(dataSize * 0.8)]
@@ -42,8 +40,8 @@ def get_data(file_location):
     X_test = np.array(normalize(test.iloc[:, :-1], minVal, maxVal))
 
     y_train = np.array(train.iloc[:, -1:])
-    y_val = np.array(val.iloc[:, -1:]).ravel()
-    y_test = np.array(test.iloc[:, -1:]).ravel()
+    y_val = np.array(val.iloc[:, -1:])
+    y_test = np.array(test.iloc[:, -1:])
 
     return X_train, X_val, X_test, y_train, y_val, y_test
 
@@ -53,27 +51,17 @@ absolutePath = r'C:\Users\gulce\Desktop\EEE 8TH SEMESTER\CS 464\Homeworks\HW2\da
 X_train, X_val, X_test, y_train, y_val, y_test = get_data(absolutePath)
 
 print("X_train shape: {}".format(X_train.shape))
-print("X_test shape: {}".format(X_test.shape))
 print("X_val shape: {}".format(X_val.shape))
+print("X_test shape: {}".format(X_test.shape))
 print("y_train shape: {}".format(y_train.shape))
-print("y_test shape: {}".format(y_test.shape))
 print("y_val shape: {}".format(y_val.shape))
+print("y_test shape: {}".format(y_test.shape))
 
-"""
-clf = l.LogisticRegression()
-clf.fit(X_train, y_train)
+batchSize = [42000, 64, 1]
+for size in batchSize:
+    print('\nStart training for batchsize =', size, '\n')
+    model = LR.LogisticRegression(epochs=100, learningRate=0.001, batchSize=size)
+    w, b = model.fit(X_train, y_train)
+    y_pred = model.predict(w, b, X_val, 0.5)
+    print('\nAcc for batchsize =', size, 'is:', accuracy_score(y_pred, y_val))
 
-score = clf.score(X_test, y_test)
-print(score)"""
-ones = np.ones((1, X_train.shape[0]))
-X_train = np.insert(X_train, 0, ones, axis=1)
-print(X_train.shape)
-print(X_train)
-
-model = LR.LogisticRegression(10, 1)
-w = model.fit(X_train, y_train)
-Y_prediction_train = model.predict(w, X_train)
-
-print(Y_prediction_train)
-accuracyBool = (Y_prediction_train == y_train)
-print('Accuracy: ', np.count_nonzero(accuracyBool) / accuracyBool.shape[0])
